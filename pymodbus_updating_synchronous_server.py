b5e053f02e54434ad74c5894bbe2df618a35bf14
Francisco Oliveto <franciscoliveto@gmail.com>
2019-10-24 19:12:30 -0300

    Updating mechanism now uses threads.

--- pymodbus_updating_synchronous_server.py
+++ pymodbus_updating_synchronous_server.py
@@ -7,14 +7,10 @@ from pymodbus.datastore import ModbusSequentialDataBlock
 from pymodbus.datastore import ModbusSlaveContext, ModbusServerContext
 from pymodbus.transaction import ModbusRtuFramer, ModbusAsciiFramer
 
-# --------------------------------------------------------------------------- #
-# import the twisted libraries we need
-# --------------------------------------------------------------------------- #
-from twisted.internet.task import LoopingCall
+from threading import Thread
 
-# --------------------------------------------------------------------------- #
-# configure the service logging
-# --------------------------------------------------------------------------- #
+import time
+
 import logging
 logging.basicConfig()
 log = logging.getLogger()
@@ -23,24 +19,23 @@ log.setLevel(logging.DEBUG)
 # --------------------------------------------------------------------------- #
 # define your callback process
 # --------------------------------------------------------------------------- #
-
-
-def updating_writer(a):
+def thread_update_context(c):
     """ A worker process that runs every so often and
     updates live values of the context. It should be noted
     that there is a race condition for the update.
 
     :param arguments: The input arguments to the call
     """
-    log.debug("updating the context")
-    context = a[0]
     register = 3
     slave_id = 0x00
     address = 110
-    values = context[slave_id].getValues(register, address, count=5)
-    values = [v + 1 for v in values]
-    log.debug("new values: " + str(values))
-    context[slave_id].setValues(register, address, values)
+    while True:
+        log.debug("updating the context")
+        values = c[slave_id].getValues(register, address, count=5)
+        values = [v + 1 for v in values]
+        log.debug("new values: " + str(values))
+        c[slave_id].setValues(register, address, values)
+        time.sleep(5)
 
 
 def run_updating_server():
@@ -65,13 +60,10 @@ def run_updating_server():
     identity.ProductName = 'pymodbus Server'
     identity.ModelName = 'pymodbus Server'
     identity.MajorMinorRevision = '2.2.0'
-    
-    # ----------------------------------------------------------------------- # 
-    # run the server you want
-    # ----------------------------------------------------------------------- # 
-    time = 5  # 5 seconds delay
-    loop = LoopingCall(f=updating_writer, a=(context,))
-    loop.start(time, now=False) # initially delay by time
+
+    x = Thread(target=thread_update_context, args=(context,), daemon=True)
+    x.start()
+
     StartTcpServer(context, identity=identity, address=("localhost", 5020))
 
 if __name__ == "__main__":
