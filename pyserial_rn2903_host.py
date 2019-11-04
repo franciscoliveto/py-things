74b254f75fa6cfb302a47b4e4256160147ab8073
Francisco Oliveto <franciscoliveto@gmail.com>
2019-11-04 13:04:03 -0300

    A new function to receive data packets and some improvements.

--- pyserial_rn2903_host.py
+++ pyserial_rn2903_host.py
@@ -35,28 +35,45 @@ def radio_set(handler, parameter, value):
 def radio_get(parameter):
     pass
 
-def radio_receive_packet():
-    pass
+def radio_receive_packet(handler: Serial, window_size: int = 0) -> str:
+    """Receives a data packet from the radio module.
 
-def radio_transmit_packet(handler: Serial, data) -> bool:
+    It blocks waiting for a valid data packet. In case of error or time out
+    an exception will be raised. If reception was successful an ASCII string 
+    containing the hexadecimal representation of the data that was received
+    will be returned.
+    """
+    ws = to_bytes(window_size)
+    command = b'radio rx ' + ws + b'\r\n'
+    handler.write(command)
+    response = handler.readline() # Wait for the first response.
+    if b'busy' in response:
+        handler.write(b'sys reset\r\n')
+        handler.readline() # dummy read()
+        return ''
+        # TODO: raise an exception.
+    elif b'invalid_param' in response:
+        pass
+    elif b'ok' in response:
+        while True:
+            response = handler.read_all()
+            if b'radio_rx' in response:
+                data = response.split()[1]
+                return data.decode()
+            elif b'radio_err' in response:
+                # TODO: raise an exception.
+                pass
+            time.sleep(1) # TODO: seconds or milliseconds yield?
+    else:
+        pass
+
+def radio_transmit_packet(handler: Serial, data: str) -> bool:
     """Transmits a data packet through the radio module.
 
     <data> is the hexadecimal value representing the data to be
-    transmitted, from 0 to 255 for LoRa modulation.
-
-    'radio tx <data>'
-
-    First response after entering the command:
-    ok - if parameter is valid and the transceiver is configured in Transmit mode
-    invalid_param - if parameter is not valid
-    busy - if the transceiver is currently busy
-    Second response after effective transmission:
-    radio_tx_ok - if transmission was successful
-    radio_err - if transmission was unsuccesful
+    transmitted.
     """
-    d = to_bytes(to_bytes(data).hex())
-    # TODO: '125' -> '313235' or '125' -> '4D'?
-    command = b'radio tx ' + d + b'\r\n'
+    command = b'radio tx ' + data.encode() + b'\r\n'
     handler.write(command)
     response = handler.readline() # Wait for the first response.
     if response == b'invalid_param\r\n' or response == b'busy\r\n':
@@ -72,8 +89,9 @@ def radio_transmit_packet(handler: Serial, data) -> bool:
     else: # Unknown response or empty
         return False
 
+        
 
-handler = Serial('/dev/ttyACM1', baudrate=57600, timeout=5)
+handler = Serial('/dev/ttyACM0', baudrate=57600, timeout=5)
 
 radio_set(handler, 'mod', 'lora')
 radio_set(handler, 'freq', 916800000)
@@ -86,8 +104,13 @@ radio_set(handler, 'sync', '12')
 radio_set(handler, 'bw', 125)
 mac_pause(handler)
 
+n = 16
 while True:
-    radio_transmit_packet(handler, 'hello')
+    hex_ascii = hex(n)[2:]
+    radio_transmit_packet(handler, hex_ascii)
+    n = (n % 65536) + 3
+    if n < 16:
+        n = 16
     time.sleep(2)
 
 
